c40571b2116fde0887f3985c9eca88fafe47c4ba
surajrawat <surajrawat95699@gmail.com>
2025-06-02 20:29:45 +0530

    Project soft delete

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\User;
 use App\Models\Task;
 
 class Project extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'name',
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,7 +3,7 @@
         <div class="flex items-center justify-between">
             <div>
                 <div class="flex items-center">
-                    <a href="{{ route('dashboard') }}" class="text-indigo-600 hover:text-indigo-900 mr-2">
+                    <a href="{{ route('projects.index') }}" class="text-indigo-600 hover:text-indigo-900 mr-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                         </svg>
